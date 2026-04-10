cc931661a57f40e8607b9c195cd6b8b216b17e29
stvort <stvort@mail.ru>
2026-04-10 21:05:14 +0400

    L24-web

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,9 @@ allprojects {
     val j2objcAnnotations: String by project
     val redisson: String by project
 
+    val jetty: String by project
+    val freemarker: String by project
+
     apply(plugin = "io.spring.dependency-management")
     dependencyManagement {
         dependencies {
@@ -45,6 +48,15 @@ allprojects {
             dependency("com.google.errorprone:error_prone_annotations:$errorProneAnnotations")
             dependency("com.google.j2objc:j2objc-annotations:$j2objcAnnotations")
             dependency("org.redisson:redisson:$redisson")
+
+            dependency("org.eclipse.jetty.ee10:jetty-ee10-servlet:$jetty")
+            dependency("org.eclipse.jetty:jetty-server:$jetty")
+            dependency("org.eclipse.jetty.ee10:jetty-ee10-webapp:$jetty")
+            dependency("org.eclipse.jetty:jetty-security:$jetty")
+            dependency("org.eclipse.jetty:jetty-http:$jetty")
+            dependency("org.eclipse.jetty:jetty-io:$jetty")
+            dependency("org.eclipse.jetty:jetty-util:$jetty")
+            dependency("org.freemarker:freemarker:$freemarker")
         }
     }
 
