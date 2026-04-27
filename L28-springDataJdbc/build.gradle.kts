5e847e4f14740b01016a1fe9a4ebe02018361a53
petrelevich <petrelevich@yandex.ru>
2026-04-27 08:25:34 +0300

    L28-springDataJdbc

---
+++ L28-springDataJdbc/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("org.springframework.boot")
+}
+
+
+dependencies {
+    implementation("ch.qos.logback:logback-classic")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.postgresql:postgresql")
+    implementation("com.google.code.findbugs:jsr305")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+
+    runtimeOnly("org.flywaydb:flyway-database-postgresql")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+}
